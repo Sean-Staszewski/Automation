be78e7436672d4ce3cf64ee8df51e87b26c54db1
Sean-Staszewski <166570500+Sean-Staszewski@users.noreply.github.com>
2025-07-03 14:54:22 -0400

    Update script.py

--- script.py
+++ script.py
@@ -7,6 +7,8 @@ import subprocess
 file_name = "docking_train.ipynb"
 file = importlib.import_module(f"{file_name}")
 
+outputFileName = "Dock_TrainingT"
+
 SysResCommand = ["./SysResCollector", "write", "1000000000", "620000000000", "Dock_TrainingT1.bin"]
 fileCommand = ["jupyter", "nbconvert", "--to", "notebook", "--execute", "--inplace", "docking_train.ipynb"]
 translateCommand = ["./SysResCollector", "read", "Dock_TrainingT1.bin", "Dock_TrainingT1.csv"]
@@ -14,8 +16,16 @@ translateCommand = ["./SysResCollector", "read", "Dock_TrainingT1.bin", "Dock_Tr
 new_learning_rate = [0.005, 0.01, 0.05, 0.1, 0.5]
 new_node_size = [32, 64, 128, 256, 512]
 
-for new_batch_size in range(1,100001,1000):
+trialCount = 1
+
+for new_batch_size in range(1,100001, 1000):
     if (hasattr(file, 'batch_size')):
+        SysResCommand[4] = f"{outputFileName}{trialCount}.bin"
+        translateCommand[2] = SysResCommand[4]
+        translateCommand[3] = f"{outputFileName}{trialCount}.csv"
+        
+        ++trialCount
+        
         setattr(file, 'batch_size', new_batch_size)
         #run NN and syscollector
         subprocess.Popen(SysResCommand)
@@ -23,12 +33,16 @@ for new_batch_size in range(1,100001,1000):
         subprocess.run(translateCommand, check=true)
 
 
-SysResCommand[4] = "Dock_TrainingT2.bin"
-translateCommand[2] = SysResCommand[4]
-translateCommand[3] = "Dock_TrainingT2.csv"
+
 #set batch size to standard
 for i in range(len(new_learning_rate)):
     if (hasattr(file, 'lr')):
+        SysResCommand[4] = f"{outputFileName}{trialCount}.bin"
+        translateCommand[2] = SysResCommand[4]
+        translateCommand[3] = f"{outputFileName}{trialCount}.csv"
+
+        ++trialCount
+        
         setattr(file, 'lr', new_learning_rate[i])
         # run NN and syscollector
         subprocess.Popen(SysResCommand)
@@ -36,12 +50,16 @@ for i in range(len(new_learning_rate)):
         subprocess.run(translateCommand, check=true)
 
 
-SysResCommand[4] = "Dock_TrainingT3.bin"
-translateCommand[2] = SysResCommand[4]
-translateCommand[3] = "Dock_TrainingT3.csv"
+
 #set learning rate to standard
 for i in range(len(new_node_size)):
     if (hasattr(file, 'size')):
+        SysResCommand[4] = f"{outputFileName}{trialCount}.bin"
+        translateCommand[2] = SysResCommand[4]
+        translateCommand[3] = f"{outputFileName}{trialCount}.csv"
+
+        ++trialCount
+        
         setattr(file, 'size', new_node_size[i])
         #run NN and syscollector
         subprocess.Popen(SysResCommand)
